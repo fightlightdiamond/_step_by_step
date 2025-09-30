5985863723346da6b7372572070966d7c9863222
Your Name <you@example.com>
2025-09-30 10:03:41 +0700

    chore: get base path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-
+import { getBasePath } from '@/utils';
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={getBasePath('/next.svg')}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src={getBasePath('/vercel.svg')}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -60,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src={getBasePath('/file.svg')}
             alt="File icon"
             width={16}
             height={16}
@@ -75,7 +75,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src={getBasePath('/window.svg')}
             alt="Window icon"
             width={16}
             height={16}
@@ -90,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src={getBasePath('/globe.svg')}
             alt="Globe icon"
             width={16}
             height={16}
