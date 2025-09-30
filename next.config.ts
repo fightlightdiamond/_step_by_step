a56580d186c905f1b6b97fc4696094eb56c219e0
Your Name <you@example.com>
2025-09-30 09:46:46 +0700

    feat: base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // Read from .env file
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
